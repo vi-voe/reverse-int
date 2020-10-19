522ca8bef82444f7e71c9b9a270805c23a4cd678
ext_vvoevodkina <vivoe@yandex.ru>
2020-10-19 17:00:46 +0300

    feat: make reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let str = n.toString();
+  let newNum =''; 
+  for(let i=str.length-1; i > -1; i--) {
+    newNum+=str.charAt(i)
+  }
+  console.log(newNum)
+  return Number.parseInt(newNum)
 }
